404b6ae2e4c46fe39a9f5a6580966c764dad69d3
Arya Nazare <arya.nazare29@gmail.com>
2026-09-16 12:34:35 +0530

    Fix router base path for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function ThemeApplier({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/budgetup/'>
       <SettingsProvider>
         <ThemeApplier>
           <ToastProvider>
